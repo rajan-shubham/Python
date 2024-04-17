2dd5c334d9e63b382cc74c513897102ccf35f400
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-04-18 00:54:54 +0530

    print a list of 10 randomly generated 5 character strings

--- exam_practice/rand10strof5char.py
+++ exam_practice/rand10strof5char.py
@@ -5,3 +5,12 @@ random_strings = [''.join(random.choices(string.ascii_letters + string.digits, k
 
 # Print the list
 print(random_strings)
+
+# Write Python code to print a list of 10 randomly generated 5 character strings. Your code Must use list comprehension. you may use the 'choice' function from the 'random' python library.
+def string_lengths(input_list):
+    return [len(string) for string in input_list]
+
+# Example usage:
+input_strings = ["hello", "world", "python", "list", "comprehension"]
+result_lengths = string_lengths(input_strings)
+print(result_lengths)
