6b88ec6565c7a96cdd96a5cfbe1b44190e1735df
shubham kumar <128181591+rajan-shubham@users.noreply.github.com>
2024-11-04 17:47:31 +0530

    differentOperatorin python

---
+++ basics/differentOperatorin.py
@@ -0,0 +1,31 @@
+a = 7
+b = 2
+
+# addition
+print ('Sum: ', a + b)  
+
+# subtraction
+print ('Subtraction: ', a - b)   
+
+# multiplication
+print ('Multiplication: ', a * b)  
+
+# division
+print ('Division: ', a / b) 
+
+# floor division
+print ('Floor Division: ', a // b)
+
+# modulo
+print ('Modulo: ', a % b)  
+
+# a to the power b
+print ('Power: ', a ** b)   
+
+# Sum: 9
+# Subtraction: 5
+# Multiplication: 14
+# Division: 3.5
+# Floor Division: 3
+# Modulo: 1
+# Power: 49
