dfc05da6e1c6921243eafb34136e2cb3543260b2
rajan-shubham <SHUBHAMRAJAN@proton.me>
2024-12-23 15:29:37 +0530

    swap in python

---
+++ basics/swap.py
@@ -0,0 +1,14 @@
+
+var1 = input ("Enter the first variable ")
+
+var2 = input ("Enter the second variable ")
+
+temp = var1
+
+var1 = var2 
+
+var2 = temp
+
+print ("The value of the first variable after swapping is:", var1)
+
+print ("The value of the second variable after swapping is:", var2)
